83ee6e25330a0b6da40919b88382ba3c25947ce3
Rafael do Nascimento Ribeiro <rafaelnribeiro@outlook.com>
2015-09-28 18:36:30 -0300

    Adicionado função token

--- drive_bares.cpp
+++ drive_bares.cpp
@@ -2,6 +2,41 @@
 #include <fstream>
 #include <utility>
 #include <string>
+#include <vector>
+
+std::string digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+std::string symbols = {'(', ')', '^', '*', '/', '%', '-', '+'};
+
+
+
+std::vector<std::string> token(std::string str)
+{
+	std::string num;
+	std::vector<std::string> exps;
+	for(char c: str)
+	{
+		if (digits.find(c) != std::string::npos)
+		{
+			num.push_back(c);
+		}
+		else if(symbols.find(c) != std::string::npos)
+		{
+			if(num.size() > 0)
+			{
+				exps.push_back(num);
+				num.resize(0);
+			}
+			std::string s(1,c);
+			exps.push_back(s);
+		}
+		else if(!isspace(c))
+		{
+			std::cout << std::endl << "Símbolo "<< c <<" é inválido!" << std::endl;
+		}
+	}
+	return exps;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -32,6 +67,13 @@ int main(int argc, char const *argv[])
 	while(!myStream.eof())
 	{
 		getline(myStream, expression);
+		std::vector<std::string> exps = token(expression);
+		for (std::string e: exps)
+		{
+			std::cout << "{" << e << "} ";
+		}
+		std::cout << std::endl;
 	}
+
 	return 0;
 }
