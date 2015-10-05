a9655a589599b7495b02b95dbe9a92d5f8daf8cc
Rafael do Nascimento Ribeiro <rafaelnribeiro@outlook.com>
2015-10-05 17:38:17 -0300

    Adicionada função calcPosfix

--- bares.cpp
+++ bares.cpp
@@ -23,31 +23,105 @@ struct token
 	int position;
 };
 
+//Calcula uma expressão posfixa
+int calcPosfix(QueueLL<token> exps)
+{
+	StackLL<int> stack; //Pilha de inteiros
+	token simb; //Token atual
+	int op1, op2, result;
 
+	while(!exps.empty())
+	{
+		simb = exps.remove(); //Remove um token da fila
+
+		//Se for um operando
+		if(simb.type == OPERAND)
+		{
+			int n = stoi(simb.info);
+			//Adiciona o valor à pilha
+			stack.push(n);
+		}
+		//Se for um operador
+		else
+		{
+			//Retira os dois ultimos valores da pilha
+			op2 = stack.pop();
+			op1 = stack.pop();
+
+			char c = simb.info[0];
+
+			//Realiza a operação baseada no operador
+			switch(c)
+			{
+				case '+':
+					result = op1 + op2;
+					break;
+
+				case '-':
+					result = op1 - op2;
+					break;
+					
+				case '*':
+					result = op1 * op2;
+					break;
+					
+				case '/':
+					if(op2 == 0)
+					{
+						std::cout << "Erro coluna " << simb.position << ": Divisão por zero!" << std::endl;
+						return 0;
+					}
+					result = op1 / op2;
+					break;
+					
+				case '%':
+					result = op1 % op2;
+					break;
+					
+				case '^':
+					result = op1 ^ op2;
+					break;
+			}
+			//Adiciona o resulatado à pilha
+			stack.push(result);
+		}
+	}
+	result = stack.pop();
+	return result;
+}
+
+//Cria uma fila de tokens
 void stringToInfix(std::string str, QueueLL<token> & exps)
 {
 	unsigned int i = 0;
+	//Lê a string
 	while(i < str.size())
 	{
+		//Se for um dígito
 		if(digits.find(str[i]) != std::string::npos)
 		{
 			std::string num;
-			int ini = i;
+			int ini = i;// Posição do primeiro dígito
 
+			//Concatena todos os digitos que estão juntos
 			while(digits.find(str[i]) != std::string::npos)
 			{
 				num.push_back(str[i]);
 				++i;
 			}
 
+			//Adiciona o número a um token
 			token tmp;
 			tmp.info = num;
 			tmp.type = OPERAND;
 			tmp.isValid = true;
 			tmp.position = ini;
+
+			//Adiciona o token à fila
 			exps.insert(tmp);
 		}
 
+		//Se não for um caractere nulo e não chegou o final da string
 		if(!isspace(str[i]) && str[i] != '\0')
 		{
 			std::string s(1,str[i]);
@@ -56,21 +130,25 @@ void stringToInfix(std::string str, QueueLL<token> & exps)
 			tmp.info = s;
 			tmp.position = i;
 
+			//Se for um operador válido
 			if(valOperators.find(str[i]) != std::string::npos)
 			{
 				tmp.type = OPERATOR;
 				tmp.isValid = true;
 			}
+			//Se for um operador inválido
 			else if(invOperators.find(str[i]) != std::string::npos)
 			{
 				tmp.type = OPERATOR;
 				tmp.isValid = false;
 			}
+			//Caso contrário, é um operando inválido
 			else
 			{
 				tmp.type = OPERAND;
 				tmp.isValid = false;
 			}
+			//Adiciona o token à fila
 			exps.insert(tmp);
 		}
 		++i;
