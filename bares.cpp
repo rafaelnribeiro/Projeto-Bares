3826e3a806d31673f7477bd24cb1ef2783e6c693
Rafael do Nascimento Ribeiro <rafaelnribeiro@outlook.com>
2015-10-06 19:01:26 -0300

    Adicionado todos os tratamentos de erros restantes

--- bares.cpp
+++ bares.cpp
@@ -23,78 +23,7 @@ struct token
 	int position;
 };
 
-//Calcula uma expressão posfixa
-bool calcPosfix(QueueLL<token> & exps, int & result)
-{
-	if (exps.empty())
-		return false;
 
-	StackLL<int> stack; //Pilha de inteiros
-	token simb; //Token atual
-	int op1, op2;
-
-
-	while(!exps.empty())
-	{
-		simb = exps.remove(); //Remove um token da fila
-		//std::cout << simb.info << std::endl;
-		//Se for um operando
-		if(simb.type == OPERAND)
-		{
-			int n = stoi(simb.info);
-			//Adiciona o valor à pilha
-			stack.push(n);
-		}
-		//Se for um operador
-		else
-		{
-			//Retira os dois ultimos valores da pilha
-			op2 = stack.pop();
-			op1 = stack.pop();
-
-			char c = simb.info[0];
-
-			//Realiza a operação baseada no operador
-			switch(c)
-			{
-				case '+':
-					result = op1 + op2;
-					break;
-
-				case '-':
-					result = op1 - op2;
-					break;
-
-				case '*':
-					result = op1 * op2;
-					break;
-
-				case '/':
-					if(op2 == 0)
-					{
-						std::cout << "Erro coluna " << simb.position << ": Divisão por zero!" << std::endl;
-						return false;
-					}
-					result = op1 / op2;
-					break;
-
-				case '%':
-					result = op1 % op2;
-					break;
-
-				case '^':
-					result = op1 ^ op2;
-					break;
-			}
-			//Adiciona o resulatado à pilha
-			stack.push(result);
-		}
-	}
-	if (!stack.empty())
-		result = stack.pop();
-
-	return true;
-}
 
 int comparePref (std::string opr) {
 	char c = opr[0];
@@ -192,30 +121,29 @@ void stringToInfix(std::string str, QueueLL<token> & exps)
 		++i;
 	}
 }
+
 //Transforma uma expressão infixa em posfixa
 bool infixToPosfix (QueueLL<token> & exps, QueueLL<token> & posFix) {
 
-	bool cont = true;
+	bool sucess = true;
 
 	StackLL<token> stk;
 
-	SimbolType expected = OPERAND;
+	SimbolType expected = OPERAND;//Tipo de token esperado no momento
 	int pos = 0;
+	bool first = true;
+
+	StackLL<int> stkPrt;//Pilha para tratar o erro de escopo aberto
 
 	while(!exps.empty())
 	{
 		token tmp = exps.remove();
+
+		//Se já apareceu pelo menos um número
+		if(tmp.type == OPERAND)
+			first = false;
 		
-		if (tmp.isValid == false)
-		{
-			if (tmp.type == OPERAND)
-				std::cout << "Erro coluna " << tmp.position << ": Operando '" << tmp.info << "' é inválido!" << std::endl;
-			else
-				std::cout << "Erro coluna " << tmp.position << ": Operador '" << tmp.info << "' é inválido!" << std::endl;
-
-			cont = false;
-		}
-
+		//Tratamento para o erro de falta de operando/operador
 		if(tmp.type != expected && tmp.info[0] != ')' && tmp.info[0] != '(')
 		{
 			if(expected == OPERATOR)
@@ -224,47 +152,184 @@ bool infixToPosfix (QueueLL<token> & exps, QueueLL<token> & posFix) {
 			}
 			else
 			{
+				//Se ,na lista, o primeiro operador aparecer antes do primeiro operando
+				if(first)
+					pos = tmp.position;
+
 				std::cout << "Erro coluna " << pos << ": Falta operando"<< std::endl;
 			}
-			cont = false;
+			sucess = false;
 		}
+
+		//Tratamento para erro de operador/operando inválido
+		if (!tmp.isValid)
+		{
+			if (tmp.type == OPERAND)
+				std::cout << "Erro coluna " << tmp.position << ": Operando '" << tmp.info << "' é inválido!" << std::endl;
+			else
+				std::cout << "Erro coluna " << tmp.position << ": Operador '" << tmp.info << "' é inválido!" << std::endl;
+
+			sucess = false;
+		}
+
+
 
 		if (tmp.type == OPERAND)
 		{
+			//Tratamento para erro de constante numérica inválida
+			if(tmp.isValid && stoi(tmp.info) > 32767)
+			{
+				std::cout << "Erro coluna " << tmp.position << ": Operando '" << tmp.info << "' está fora da faixa permitida!" << std::endl;
+
+			}
+
+			//Operandos vão direto para a fila
 			posFix.insert(tmp);
 			expected = OPERATOR;
 		}
 		else if (tmp.info[0] == '(')
 		{
+			//Empilha o '('
 			stk.push(tmp);
+
+			//Adiciona a posição do parenteses à pilha de inteiros
+			stkPrt.push(tmp.position);
+
 		}
 		else if (tmp.info[0] == ')')
 		{
-			while (stk.top().info[0] != '(')
+			//Tratamento de erro para fechamento de escopo inválido
+			if(stkPrt.empty())
 			{
-				posFix.insert(stk.pop());
+				std::cout << "Erro coluna " << tmp.position << ": Fechamento de escopo inválido" << std::endl;
+				sucess = false;
 			}
-			stk.pop();
+			else
+			{
+				stkPrt.pop();
+
+				//Desempilha todos os tokens até encontrar um '('
+				while (!stk.empty() && stk.top().info[0] != '(')
+				{
+					posFix.insert(stk.pop());
+				}
+
+				//Desempilha o '('
+				if(!stk.empty())
+				stk.pop();	
+			}
 		}
 		else
 		{
+			//Desempilha todos os operandos de precedência maior ou igual ao atual os adiciona a fila
 			while (!stk.empty() && stk.top().info[0] != '(' && comparePref(stk.top().info) <= comparePref(tmp.info))
 			{
 				posFix.insert(stk.pop());
 			}
+			//Empilha o operador atual
 			stk.push(tmp);
+
 			expected = OPERAND;
 		}
 		pos = tmp.position;
 	}
 
+	//Desempilha os operadores restantes e os adiciona a fila
 	while (!stk.empty())
 	{
 		posFix.insert(stk.pop());
 	}
 
-	return cont;
+	//Tratamento para erro de escopo aberto
+	while(!stkPrt.empty())
+	{
+		std::cout << "Erro coluna " << stkPrt.pop() << ": Escopo aberto" << std::endl;
+		sucess = false;
+	}
 
+	//Caso a fila termine esperando um operando
+	if(expected == OPERAND)
+	{
+		std::cout << "Erro coluna " << pos << ": Falta operando"<< std::endl;
+		sucess = false;
+	}
+
+	return sucess;
+
+}
+
+//Calcula uma expressão posfixa
+bool calcPosfix(QueueLL<token> & exps, int & result)
+{
+	if (exps.empty())
+		return false;
+
+	StackLL<int> stack; //Pilha de inteiros
+	token simb; //Token atual
+	int op1, op2;
+
+
+	while(!exps.empty())
+	{
+		simb = exps.remove(); //Remove um token da fila
+		//std::cout << simb.info << std::endl;
+		//Se for um operando
+		if(simb.type == OPERAND)
+		{
+			int n = stoi(simb.info);
+			//Adiciona o valor à pilha
+			stack.push(n);
+		}
+		//Se for um operador
+		else
+		{
+			//Retira os dois ultimos valores da pilha
+			op2 = stack.pop();
+			op1 = stack.pop();
+
+			char c = simb.info[0];
+
+			//Realiza a operação baseada no operador
+			switch(c)
+			{
+				case '+':
+					result = op1 + op2;
+					break;
+
+				case '-':
+					result = op1 - op2;
+					break;
+
+				case '*':
+					result = op1 * op2;
+					break;
+
+				case '/':
+					//Tratamento de erro de divisão por zero
+					if(op2 == 0)
+					{
+						std::cout << "Erro coluna " << simb.position << ": Divisão por zero!" << std::endl;
+						return false;
+					}
+					result = op1 / op2;
+					break;
+
+				case '%':
+					result = op1 % op2;
+					break;
+
+				case '^':
+					result = op1 ^ op2;
+					break;
+			}
+			//Adiciona o resulatado à pilha
+			stack.push(result);
+		}
+	}
+	if (!stack.empty())
+		result = stack.pop();
+
+	return true;
 }
 
 int main(int argc, char const *argv[])
@@ -299,7 +364,7 @@ int main(int argc, char const *argv[])
 		int result;
 
 		getline(myStream, expression);
-		//std::cout << "Chegou aqui 'getline()'" << std::endl;
+
 		stringToInfix(expression, exps);
 		
 		if (infixToPosfix(exps, posFix))
@@ -309,15 +374,7 @@ int main(int argc, char const *argv[])
 				std::cout << result << std::endl;	
 			}
 		}
-		
-
-
-		/*while(!posFix.empty())
-		{
-			token tmp = posFix.remove();
-			std::cout << "{ " << tmp.info << ", " << tmp.type << ", " << tmp.position << ", " << tmp.isValid << "} ";
-		}
-		std::cout << std::endl;*/
+		std::cout << std::endl;
 	}
 
 	return 0;
